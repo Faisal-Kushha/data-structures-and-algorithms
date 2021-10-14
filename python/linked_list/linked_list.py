02df23309120c653a595fb715ae041128f3c8a0b
Faisal-Kushha <fkushha89@hotmail.com>
2021-10-14 23:13:20 +0300

    done

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,12 +1,46 @@
+class Node:
+    def __init__(self, value, next_=None):
+        self.value = value
+        self.next_ = next_
+
+
 class LinkedList:
     """
     Put docstring here
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.head = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def insert(self, value):
+        self.head = Node(value, self.head)
+
+    def includes(self, value):
+        the_list = self.head
+        while the_list:
+            if the_list.value == value:
+                return True
+            the_list = the_list.next_
+        return False
+
+    def __str__(self):
+        output = ""
+        the_list = self.head
+        while the_list:
+            value = the_list.value
+            # if the_list.next_ == None:
+            output = output + f"( {value} ) -> NULL"
+            # else:
+            the_list = the_list.next_
+            # output = output + f"( {value} ) -> "
+
+        return output
+
+
+if __name__ == "__main__":
+    ll = LinkedList()
+    ll.insert(1)
+    ll.insert(2)
+    ll.insert(3)
+    ll.insert(4)
+    ll.__str__()
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,71 @@
-from linked_list.linked_list import LinkedList
+from linked_list.linked_list import Node, LinkedList
+import pytest
 
 
 def test_import():
     assert LinkedList
+
+
+def test_node_has_int_data():
+    expected = 1
+    node = Node(1)
+    actual = node.value
+    assert actual == expected
+
+
+def test_node_has_str_data():
+    expected = "x"
+    node = Node("x")
+    actual = node.value
+    assert actual == expected
+
+
+def test_node_is_a_Node():
+    expected = "Node"
+    node = Node(1)
+    actual = type(node).__name__
+    assert actual == expected
+
+
+def test_node_without_value():
+    with pytest.raises(TypeError):
+        node = Node()
+
+
+def test_new_linked_list_is_empty():
+    expected = None
+    x = LinkedList()
+    actual = x.head
+    assert actual == expected
+
+
+def test_linked_list_insert():
+    expected = None
+    x = LinkedList()
+    actual = x.insert(1)
+    assert actual == expected
+
+
+def test_linked_list_includes_true():
+    expected = True
+    LinkedList().insert(1)
+    LinkedList().insert(2)
+    LinkedList().insert(3)
+    actual = LinkedList().includes(1)
+    assert actual == expected
+
+
+def test_linked_list_includes_false():
+    expected = False
+    LinkedList().insert(1)
+    LinkedList().insert(2)
+    LinkedList().insert(3)
+    actual = LinkedList().includes(4)
+    assert actual == expected
+
+
+def test_return_all_values():
+    expected = '( 1 ) -> ( 2 ) -> ( 3 ) -> NULL'
+    x = LinkedList()
+    actual = x.__str__()
+    assert actual == expected
