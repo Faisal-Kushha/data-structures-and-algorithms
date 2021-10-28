8e043ec63ac50ef0b56840c689cd4ea7140af59a
Faisal-Kushha <fkushha89@hotmail.com>
2021-10-28 21:38:30 +0300

    Unfinished

---
+++ python/trees/trees.py
@@ -0,0 +1,164 @@
+"""
+This Module defines a Node and a Binary Tree
+"""
+
+
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+class Queue:
+    def __init__(self, collection=[]):
+        self.value = collection
+
+    def peek(self):
+        if len(self.value):
+            return True
+        return False
+
+    def enqueue(self, item):
+        self.value.append(item)
+
+    def dequeue(self):
+        return self.value.pop(0)
+
+
+class BinaryTree:
+    def _init_(self):
+        self.root = None
+
+    def bfs(self):
+        """
+        A binary tree method which returns a list of items that it contains
+
+        input: None
+
+        output: tree items
+        """
+        # Queue queue <-- new Queue()
+        queue = Queue()
+        # queue.enqueue(root)
+        queue.enqueue(self.root)
+
+        list_of_items = []
+
+        while queue.peek():
+            front = queue.dequeue()
+            list_of_items += [front.value]
+
+            if front.left:
+                queue.enqueue(front.left)
+
+            if front.right:
+                queue.enqueue(front.right)
+
+        return list_of_items
+
+    def pre_order(self):
+        """
+        A binary tree method which returns a list of items that it contains
+
+        input: None
+
+        output: tree items
+
+        sub method : walk () to make the recursion staff
+        """
+        list_of_items = []
+
+        def walk(node):
+            if node:
+                list_of_items.append(node.value)
+                if node.left:
+                    walk(node.left)
+                if node.right:
+                    walk(node.right)
+            else:
+                return "Empty tree"
+
+        walk(self.root)
+        return list_of_items
+
+    def in_order(self):
+        """
+        function to in order the list using Trees
+        """
+        list_of_items = []
+
+        def walk(node):
+            if node:
+                if node.left:
+                    walk(node.left)
+                list_of_items.append(node.value)
+                if node.right:
+                    walk(node.right)
+            else:
+                return "Empty tree"
+
+        walk(self.root)
+        return list_of_items
+
+    def post_order(self):
+        """
+        A binary tree method which returns a list of items in post order
+
+        input: None
+
+        output: tree items
+        """
+        list_of_items = []
+
+        def walk(node):
+            if node:
+                if node.left:
+                    walk(node.left)
+                if node.right:
+                    walk(node.right)
+                list_of_items.append(node.value)
+            else:
+                return "Empty tree"
+
+        walk(self.root)
+        return list_of_items
+
+
+class BinarySearchTree(BinaryTree):
+    def add(self, value):
+
+        if self.root == None:
+            self.root = Node(value)
+        else:
+            current = self.root
+
+            while current:
+                if value < current.value:
+                    if not current.left:
+                        current.left = Node(value)
+                        break
+                    current = current.left
+                else:
+                    if not current.right:
+                        current.right = Node(value)
+                        break
+                    current = current.right
+
+    def contains(self, value):
+
+        if self.root == None:
+            return "Empty tree"
+        else:
+            current = self.root
+            while current:
+                if current.value == value:
+                    return True
+                elif value < current.value:
+                    if not current.left:
+                        return False
+                    current = current.left
+                else:
+                    if not current.right:
+                        return False
+                    current = current.right
